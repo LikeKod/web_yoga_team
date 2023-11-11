142eecd39061e1cdbf11180c6586cb9e178ba105
LikeKod <i.serebryanoy@yandex.ru>
2023-11-11 22:33:10 +0300

    start

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const people = [
+    {
+        session: "1",
+        cost: "Free",
+        describe:["Warm Up", "30 min session"],
+    },
+    {
+        session: "12",
+        cost: "12000",
+        describe:["Cardio ", "Warmup", "Thrice a week", "Nutrition ", "Couples Included"],
+    },
+    {
+        session: "16",
+        cost: "16000",
+        describe:["Cardio ", "Warmups", "4 times a week", "Nutrition ", "Couples Included"],
+    },
+    {
+        session: "20",
+        cost: "18000",
+        describe:["Cardio ", "Warmups", "5 times a week", "Nutrition ", "Couples Included"],
+    },
+]
