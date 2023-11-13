f91ad0c9766e09163952319ff61f4614382172cd
Mairans <mairanschaika@gmail.com>
2023-11-13 18:44:39 +0300

    comment

--- js/main.js
+++ js/main.js
@@ -1,22 +1,17 @@
-const people = [
-    {
-        session: "1",
-        cost: "Free",
-        describe:["Warm Up", "30 min session"],
-    },
-    {
-        session: "12",
-        cost: "12000",
-        describe:["Cardio ", "Warmup", "Thrice a week", "Nutrition ", "Couples Included"],
-    },
-    {
-        session: "16",
-        cost: "16000",
-        describe:["Cardio ", "Warmups", "4 times a week", "Nutrition ", "Couples Included"],
-    },
-    {
-        session: "20",
-        cost: "18000",
-        describe:["Cardio ", "Warmups", "5 times a week", "Nutrition ", "Couples Included"],
-    },
-]
+new Swiper('.image-slider', {
+  loop: true,
+
+  loopedSlides: 5,
+
+  navigation: {
+    nextEl: '.custom-button-next',
+    prevEl: '.custom-button-prev'
+  },
+
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'fraction',
+  },
+  slidesPerView: 1,
+  centeredSlides: true,
+});
